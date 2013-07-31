5daa2f670103b51da1418b805fc35a93b4346240
amangal <amangal@adobe.com>
2013-07-31 12:57:52 +0530

    Email Provider SDK - Updating the JSP files to use Apache license in the header. Removed hardcoded references to libs from dialogs configurations

--- sampleemailservicepackage/jcr_root/apps/mcm/sampleemailservice/components/sampleemailservicepage/init.jsp
+++ sampleemailservicepackage/jcr_root/apps/mcm/sampleemailservice/components/sampleemailservicepage/init.jsp
@@ -1,23 +1,32 @@
 <%--
-  Copyright 1997-2009 Day Management AG
-  Barfuesserplatz 6, 4001 Basel, Switzerland
-  All Rights Reserved.
 
-  This software is the confidential and proprietary information of
-  Day Management AG, ("Confidential Information"). You shall not
-  disclose such Confidential Information and shall use it only in
-  accordance with the terms of the license agreement you entered into
-  with Day.
+  *******************************************************************************
 
-  ==============================================================================
+  Copyright 2013 Adobe
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+  *******************************************************************************
 
   Init script.
 
   Draws the WCM initialization code. This is usually called by the head.jsp
   of the page. If the WCM is disabled, no output is written.
 
-  ==============================================================================
+  *******************************************************************************
 
---%><%@include file="/libs/foundation/global.jsp" %><%
+--%>
+
+<%@include file="/libs/foundation/global.jsp" %><%
 %><%@page session="false" import="com.day.cq.wcm.api.WCMMode" %>
 <cq:includeClientLib categories="cq.wcm.edit,cq.mcm"/>
--- sampleemailservicepackage/jcr_root/apps/mcm/sampleemailservice/widgets/xemailservice-clientlib/source/XEmailService.js
+++ sampleemailservicepackage/jcr_root/apps/mcm/sampleemailservice/widgets/xemailservice-clientlib/source/XEmailService.js
@@ -16,7 +16,7 @@
 if(CQ.mcm && CQ.mcm.utils.Newsletter){
 	CQ.mcm.utils.Newsletter.XEmailService = CQ.mcm.utils.Newsletter.XEmailService || {};
 	
-	CQ.mcm.utils.Newsletter.XEmailService.PUBLISH_DIALOG_PATH = "/libs/mcm/sampleemailservice/components/newsletter/publish/dialog.infinity.json";
+	CQ.mcm.utils.Newsletter.XEmailService.PUBLISH_DIALOG_PATH = "/apps/mcm/sampleemailservice/components/newsletter/publish/dialog.infinity.json";
 	 //publish dialog path, which can be customized as per requirement at the location above)
 	CQ.mcm.utils.Newsletter.publishDialogPath= CQ.mcm.utils.Newsletter.XEmailService.PUBLISH_DIALOG_PATH;
 
@@ -40,9 +40,9 @@ if(CQ.mcm && CQ.mcm.utils.Newsletter){
 
 //Dialog paths to Email service specific actions. Dialogs can be modified as required. For more information refer http://localhost:4502/crx/de/index.jsp#/libs/mcm/exacttarget/components/actionConfigurations/
 if(CQ.EmailServiceActionsConfig && CQ.EmailServiceActionsConfig.actionsDialogPath){
-	CQ.EmailServiceActionsConfig.actionsDialogPath[CQ.EmailServiceActionsConfig.ADD_SUBSCRIBER] = "/libs/mcm/sampleemailservice/components/actionConfigurations/addSubscriber/dialog.infinity.json";
-	CQ.EmailServiceActionsConfig.actionsDialogPath[CQ.EmailServiceActionsConfig.DELETE_SUBSCRIBER] = "/libs/mcm/sampleemailservice/components/actionConfigurations/deleteSubscriber/dialog.infinity.json";
-	CQ.EmailServiceActionsConfig.actionsDialogPath[CQ.EmailServiceActionsConfig.AUTO_RESPONDER] = "/libs/mcm/sampleemailservice/components/actionConfigurations/autoResponder/dialog.infinity.json";
+	CQ.EmailServiceActionsConfig.actionsDialogPath[CQ.EmailServiceActionsConfig.ADD_SUBSCRIBER] = "/apps/mcm/sampleemailservice/components/actionConfigurations/addSubscriber/dialog.infinity.json";
+	CQ.EmailServiceActionsConfig.actionsDialogPath[CQ.EmailServiceActionsConfig.DELETE_SUBSCRIBER] = "/apps/mcm/sampleemailservice/components/actionConfigurations/deleteSubscriber/dialog.infinity.json";
+	CQ.EmailServiceActionsConfig.actionsDialogPath[CQ.EmailServiceActionsConfig.AUTO_RESPONDER] = "/apps/mcm/sampleemailservice/components/actionConfigurations/autoResponder/dialog.infinity.json";
 }
 
 CQ.EmailServiceActionsConfig.XEmailService = CQ.EmailServiceActionsConfig.XEmailService ||{};
